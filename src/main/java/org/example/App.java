a681c7c4cec592b2e81dc2ea37152f13af099157
Vladimir <lil.rhymes9@gmail.com>
2023-11-12 16:16:09 +0300

    user console ui scanner

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ import org.example.Sorts.BubbleSort;
 import org.example.Sorts.SelectionSort;
 import org.example.Sorts.ShuttleSort;
 
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -22,13 +23,44 @@ public class App
         System.out.println("Выбором - 2");
         System.out.println("Челночная - 3");
 
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        ISort sortType = null;
+
+
+        boolean isWrongAnswer = false;
+        do {
+            int userChoose = scanner.nextInt();
+            switch (userChoose){
+                case 1:
+                    sortType = new BubbleSort();
+
+                    isWrongAnswer = false;
+                    break;
+                case 2:
+                    sortType = new SelectionSort();
+
+                    isWrongAnswer = false;
+                    break;
+                case 3:
+                    sortType = new ShuttleSort();
+
+                    isWrongAnswer = false;
+                    break;
+                default:
+                    System.out.println("Вы ввели неправильный вариант");
+                    System.out.println("Выберите тип сортировки!");
+                    System.out.println("Пузырёк - 1");
+                    System.out.println("Выбором - 2");
+                    System.out.println("Челночная - 3");
+
+                    isWrongAnswer = true;
+            }
+        }while (isWrongAnswer);
+
 
         Sorter sorter = new Sorter();
-        BubbleSort bubbleSort = new BubbleSort();
-        SelectionSort selectionSort = new SelectionSort();
-        ShuttleSort shuttleSort = new ShuttleSort();
-        sorter.setSortType(shuttleSort);
+        sorter.setSortType(sortType);
 
         int[] array = {56, 20, 32, 11, 67, 23, 83};
         System.out.println("Массив до сортировки: " + Arrays.toString(array));
